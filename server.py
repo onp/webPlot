72b4e14b7e4005a9de866a042472b61cd84c1a66
onp <onptrs@gmail.com>
2019-03-14 22:38:07 -0400

    add parallel plotter

--- server.py
+++ server.py
@@ -8,16 +8,25 @@ class Handler(BaseHTTPRequestHandler):
         if (self.path == '/'):
             f = open('index.html', 'rb')
             mimetype = 'text/html'
-        if (self.path == '/d3.js'):
-            f = open('d3.js', 'rb')
+        if (self.path == '/parallel'):
+            f = open('parallel.html', 'rb')
+            mimetype = 'text/html'
+        if (self.path == '/parallel_style.css'):
+            f = open('parallel_style.css', 'rb')
+            mimetype = 'text/css'
+        if (self.path == '/d3/d3.js'):
+            f = open('d3/d3.js', 'rb')
             mimetype = 'application/javascript'
         if (self.path == '/data/td1.csv'):
             f = open('data/td1.csv', 'rb')
             mimetype = 'text/csv'
+        if (self.path == '/data/iris.csv'):
+            f = open('data/iris.csv', 'rb')
+            mimetype = 'text/csv'
 
         if mimetype is not None:
             self.send_response(200)
-            self.send_header('Content-type', 'text/html')
+            self.send_header('Content-type', mimetype)
             self.end_headers()
             self.wfile.write(f.read())
         else:
@@ -27,6 +36,9 @@ class Handler(BaseHTTPRequestHandler):
             self.wfile.write(b'Invalid path')
         return
 
+
 if __name__ == '__main__':
-    sv = HTTPServer(("", 8080), Handler)
+    port = 8080
+    sv = HTTPServer(("", port), Handler)
+    print('Serving on {}'.format(port))
     sv.serve_forever()
