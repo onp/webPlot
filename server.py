a47de6d54fb84599e74e7162ced6725fecf7c5e4
onp <onptrs@gmail.com>
2019-06-10 22:03:39 -0400

    cleanup

--- server.py
+++ server.py
@@ -1,5 +1,18 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
+import pandas as pd
+
+df = pd.DataFrame([[23, 44], [238, 234], [264, 472]])
+
+def parseURL(s):
+    groups = s.split('/')
+    meth = groups[0]
+    tags = groups[1].split('&')
+    fmt = None
+    times, _, fmt = groups[2].rpartition('.')
+    times = times.split('_')
+    return [meth, tags, times, fmt]
+
 
 class Handler(BaseHTTPRequestHandler):
 
@@ -8,24 +21,32 @@ class Handler(BaseHTTPRequestHandler):
         if (self.path == '/'):
             f = open('index.html', 'rb')
             mimetype = 'text/html'
-        if (self.path == '/parallel') or (self.path == '/p'):
+        elif (self.path == '/parallel') or (self.path == '/p'):
             f = open('parallel.html', 'rb')
             mimetype = 'text/html'
-        if (self.path == '/lineplot') or (self.path == '/l'):
+        elif (self.path == '/lineplot') or (self.path == '/l'):
             f = open('lineplot.html', 'rb')
             mimetype = 'text/html'
-        if (self.path == '/parallel_style.css'):
+        elif (self.path == '/parallel_style.css'):
             f = open('parallel_style.css', 'rb')
             mimetype = 'text/css'
-        if (self.path == '/js/d3.js'):
+        elif (self.path == '/js/d3.js'):
             f = open('js/d3.js', 'rb')
             mimetype = 'application/javascript'
-        if (self.path == '/datafiles/td1.csv'):
+        elif (self.path == '/datafiles/td1.csv'):
             f = open('data/td1.csv', 'rb')
             mimetype = 'text/csv'
-        if (self.path == '/data/iris.csv'):
+        elif (self.path == '/data/iris.csv'):
             f = open('data/iris.csv', 'rb')
             mimetype = 'text/csv'
+        elif (self.path[:5] == '/dat/'):
+            print(self.path[5:])
+
+            self.send_response(200)
+            self.send_header('Content-type', 'text/csv')
+            self.end_headers()
+            self.wfile.write(df.to_csv().encode('utf-8'))
+            return
 
         if mimetype is not None:
             self.send_response(200)
